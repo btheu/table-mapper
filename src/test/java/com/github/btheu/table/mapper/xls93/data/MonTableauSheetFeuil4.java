2742974d85f6b0094797d366795f289bbcf1ea1f
benoit theunissen <benoit.theunissen@gmail.com>
2017-03-23 22:36:55 +0100

    feat(date-format):add test case

---
+++ src/test/java/com/github/btheu/table/mapper/xls93/data/MonTableauSheetFeuil4.java
@@ -0,0 +1,31 @@
+package com.github.btheu.table.mapper.xls93.data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import com.github.btheu.table.mapper.CellType;
+import com.github.btheu.table.mapper.Column;
+import com.github.btheu.table.mapper.Sheet;
+
+import lombok.Data;
+
+@Data
+@Sheet("Feuil4")
+public class MonTableauSheetFeuil4 {
+
+    @Column(value = "Numéro de rue", type = CellType.INT)
+    private int numero;
+
+    @Column("La rue")
+    private String rue;
+
+    @Column("La ville")
+    private String ville;
+
+    @Column(value = "Date création", type = CellType.DATE, format = "yyyy/MM/dd")
+    private Date création;
+
+    @Column(value = "Coût", type = CellType.BIG_DECIMAL)
+    private BigDecimal cout;
+
+}
