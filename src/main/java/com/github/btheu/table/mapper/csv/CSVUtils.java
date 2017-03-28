fc0def40b802921bb965e64b11a5c972a3a00106
Benoit Theunissen <benoit.theunissen@gmail.com>
2017-03-28 15:59:19 +0200

    feat(write-csv):first implementation of csv writing

---
+++ src/main/java/com/github/btheu/table/mapper/csv/CSVUtils.java
@@ -0,0 +1,67 @@
+package com.github.btheu.table.mapper.csv;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CSVUtils {
+
+    private static final char DEFAULT_SEPARATOR = ';';
+
+    public static void writeLine(Writer w, List<String> values) {
+        writeLine(w, values, DEFAULT_SEPARATOR, ' ');
+    }
+
+    public static void writeLine(Writer w, List<String> values, char separators) {
+        writeLine(w, values, separators, ' ');
+    }
+
+    // https://tools.ietf.org/html/rfc4180
+    private static String followCVSformat(String value) {
+
+        String result = value;
+        if (result.contains("\"")) {
+            result = result.replace("\"", "\"\"");
+        }
+        return result;
+
+    }
+
+    public static void writeLine(Writer w, List<String> values, char separators, char customQuote) {
+
+        log.debug("write line {}", values);
+        
+        try {
+            boolean first = true;
+
+            // default customQuote is empty
+
+            if (separators == ' ') {
+                separators = DEFAULT_SEPARATOR;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (String value : values) {
+                if (!first) {
+                    sb.append(separators);
+                }
+                if (customQuote == ' ') {
+                    sb.append(followCVSformat(value));
+                } else {
+                    sb.append(customQuote).append(followCVSformat(value)).append(customQuote);
+                }
+
+                first = false;
+            }
+            sb.append("\n");
+            w.append(sb.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+
+    }
+
+}
---
+++ src/main/java/com/github/btheu/table/mapper/csv/CSVWriter.java
@@ -0,0 +1,73 @@
+package com.github.btheu.table.mapper.csv;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.btheu.table.mapper.internal.Columns;
+import com.github.btheu.table.mapper.internal.Columns.Entry;
+import com.github.btheu.table.mapper.internal.Parser;
+
+public abstract class CSVWriter {
+
+    public static <T> void write(List<T> table, OutputStream os) {
+
+        try {
+            Writer writer = new OutputStreamWriter(os, "ISO-8859-1");
+
+            if (!table.isEmpty()) {
+
+                Columns columns = Parser.extractColumns(table.get(0).getClass());
+
+                writeHeader(columns, writer);
+
+                for (T item : table) {
+                    writeItem(columns, item, writer);
+                }
+            }
+
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    private static void writeHeader(Columns columns, Writer writer) {
+        List<String> values = new ArrayList<String>();
+
+        for (Entry entry : columns.getColumns()) {
+            values.add(entry.getName());
+        }
+
+        CSVUtils.writeLine(writer, values, ';', '"');
+    }
+
+    private static <T> void writeItem(Columns columns, T item, Writer writer) {
+        List<String> values = new ArrayList<String>();
+
+        try {
+            for (Entry entry : columns.getColumns()) {
+
+                Field field = entry.getField();
+
+                Object value = field.get(item);
+                if (value == null) {
+                    values.add("NULL");
+                } else {
+                    values.add(value.toString());
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+
+        CSVUtils.writeLine(writer, values, ';', '"');
+    }
+
+}
